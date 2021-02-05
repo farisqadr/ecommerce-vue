d06f6c7f30449b30455d0f85e4cbab4b47adf5fc
Faris Abdurrahman <faris.code@gmail.com>
2021-02-05 13:46:30 +0700

    related product

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -7,7 +7,7 @@
         <div class="row">
           <div class="col-lg-12">
             <div class="breadcrumb-text product-more text-left">
-              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+              <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
               <span>Detail</span>
             </div>
           </div>
@@ -34,19 +34,35 @@
                     :autoplay="true"
                     :loop="true"
                   >
-                    <div class="pt active" data-imgbigurl="img/mickey1.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[0])"
+                      :class="thumbs[0] == sumberimg ? 'active' : ''"
+                    >
                       <img src="img/mickey1.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey2.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[1])"
+                      :class="thumbs[1] == sumberimg ? 'active' : ''"
+                    >
                       <img src="img/mickey2.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey3.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[2])"
+                      :class="thumbs[2] == sumberimg ? 'active' : ''"
+                    >
                       <img src="img/mickey3.jpg" alt="" />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey4.jpg">
+                    <div
+                      class="pt"
+                      @click="changeImage(thumbs[3])"
+                      :class="thumbs[3] == sumberimg ? 'active' : ''"
+                    >
                       <img src="img/mickey4.jpg" alt="" />
                     </div>
                   </carousel>
@@ -96,6 +112,9 @@
       </div>
     </section>
     <!-- Product Shop Section End -->
+
+    <RelatedProduct />
+
     <FooterHilya />
   </div>
 </template>
@@ -105,6 +124,8 @@
 //import HelloWorld from "@/components/HelloWorld.vue";
 import HeaderHilya from "@/components/HeaderHilya.vue";
 import FooterHilya from "@/components/FooterHilya.vue";
+
+import RelatedProduct from "@/components/RelatedProduct.vue";
 import carousel from "vue-owl-carousel";
 
 export default {
@@ -113,11 +134,24 @@ export default {
     HeaderHilya,
     FooterHilya,
     carousel,
+
+    RelatedProduct,
   },
   data() {
     return {
       sumberimg: "img/mickey2.jpg",
+      thumbs: [
+        "img/mickey1.jpg",
+        "img/mickey2.jpg",
+        "img/mickey3.jpg",
+        "img/mickey4.jpg",
+      ],
     };
+  },
+  methods: {
+    changeImage(urlImage) {
+      this.sumberimg = urlImage;
+    },
   },
 };
 </script>
